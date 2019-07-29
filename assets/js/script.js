66bf97212f9a9a5aa289ce2b984df4af0242585a
Shajan Jacob <shajanjp@gmail.com>
2019-07-29 13:55:07 +0530

    adds card sort and game event handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 var socket = io('http://localhost:3000');
+let CARD_SET = ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJ", "SQ", "SK", "SA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK", "HA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK", "DA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "CA"];
+
+function cardSort(a, b) {
+  return (CARD_SET.indexOf(a) > CARD_SET.indexOf(b));
+}
 
 function newCards(data){
   let cardsContainer = document.getElementById("cards-container");
   let cardsList = '';
-  data.cards.forEach((card) => {
-    cardsList += ` <a class="orange card">
+  data.cards.sort(cardSort).forEach((card) => {
+    cardsList += ` <a class="grey card">
     <div class="fluid image">
     <img src='./assets/img/${card}.jpg'>
     </div>
@@ -17,12 +22,17 @@ function requestNewGame(){
   socket.emit('NEW_GAME');  
 }
 
+function requestJoinGame(){
+  socket.emit('JOIN', { username: 'PlAYER1'} );
+}
+
 socket.on('CARD_SHUFFLED', newCards);
-
 socket.on('NEW_GAME', requestNewGame);
-
-socket.emit('JOIN', { username: 'shajan'} );
 
 document.getElementById('new-game').onclick = () => {
   requestNewGame();
+}
+
+document.getElementById('join-game').onclick = () => {
+  requestJoinGame();
 }
