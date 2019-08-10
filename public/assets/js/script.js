1acbb72c2c64cd8f1e2d98c9b64fc17289d04360
Shajan Jacob <shajanjp@gmail.com>
2019-08-10 22:06:00 +0530

    adds support for sessionId on socket connect

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,9 @@
-var socket = io();
+let sectionId = localStorage.getItem('sectionId') || '0';
+let socketQuery = {
+  query: `sessionId=${sectionId}`
+}
+
+var socket = io.connect('', socketQuery);
 let CARD_SET = ["SA","SK","SQ","SJ","S10","S9","S8","S7","S6","S5","S4","S3","S2","DA","DK","DQ","DJ","D10","D9","D8","D7","D6","D5","D4","D3","D2","CA","CK","CQ","CJ","C10","C9","C8","C7","C6","C5","C4","C3","C2","HA","HK","HQ","HJ","H10","H9","H8","H7","H6","H5","H4","H3","H2"];
 let usernameLastSet = localStorage.getItem('username')
 
